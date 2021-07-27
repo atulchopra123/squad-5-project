d00cc02495ef34ddb4f689591c359ed5012e5825
Tristan <tristan.picotte@ibm.com>
2021-07-27 10:55:53 -0600

    Fixed Failing App Test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Deployment/i);
+  const linkElement = screen.getByText(/Deployments/i);
   expect(linkElement).toBeInTheDocument();
 });
