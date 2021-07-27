cfe8c9203191cd451ceaf092586d4c3f33f41df2
Atul Chopra <atul.chopra@ibm.com>
2021-07-27 07:45:23 -0700

    Fix for recovery time compile error.

--- src/RecoveryTimes.test.js
+++ src/RecoveryTimes.test.js
@@ -3,6 +3,6 @@ import RecoveryTimes from './RecoveryTimes';
 
 test('renders RecoveryTimes', () => {
   render(<RecoveryTimes />);
-  const linkElement = screen.getByText(/Recovery Times/);
+  const linkElement = screen.getByText(/Recovery Times/i);
   expect(linkElement).toBeInTheDocument();
 });
