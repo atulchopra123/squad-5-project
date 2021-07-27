dfa851f91309fa795879642e3c870f9eb824bee8
Anthony Stevens <apsteven@us.ibm.com>
2021-07-27 09:19:05 -0700

    checkin tests

--- src/RecoveryTimes.test.js
+++ src/RecoveryTimes.test.js
@@ -6,3 +6,21 @@ test('renders RecoveryTimes', () => {
   const linkElement = screen.getByText(/Recovery Times/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Start Time Input', () => {
+  render(<RecoveryTimes />);
+  const linkElement = screen.getByText(/Start Time:/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Start Date Input', () => {
+  render(<RecoveryTimes />);
+  const linkElement = screen.getByText(/Start Date:/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Duration Input', () => {
+  render(<RecoveryTimes />);
+  const linkElement = screen.getByText(/Duration:/i);
+  expect(linkElement).toBeInTheDocument();
+});
