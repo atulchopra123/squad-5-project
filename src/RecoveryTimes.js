5346eb10b90a8417a94392be1863381dab6be041
Anthony Stevens <apsteven@us.ibm.com>
2021-07-28 09:33:38 -0700

    Updated date/time functions

--- src/RecoveryTimes.js
+++ src/RecoveryTimes.js
@@ -26,20 +26,27 @@ function RecoveryTimes() {
     localStorage.setItem("recoveries_json", JSON.stringify(recoveries));
   }, [recoveries])
 
-  function formatDate(date, time) {
-    const utcSeconds = new Date(...`${date} ${time}`.split(/[- :]/)).getTime() / 1000;
-    const d = new Date(0);
-    d.setUTCSeconds(utcSeconds);
-    return d.toLocaleDateString("en-US");
-  }
+  function jsDate(date, time) {
+    var dt = new Date(...`${date} ${time}`.split(/[- :]/));
+    // BUG?? - Month seems to be off by 1...
+    dt.setMonth(dt.getMonth() - 1);
+    return dt;
+  }
 
-  function formatTime(date, time) {
-    const utcSeconds = new Date(...`${date} ${time}`.split(/[- :]/)).getTime() / 1000;
-    const d = new Date(0);
-    d.setUTCSeconds(utcSeconds);
-    return d.toLocaleTimeString("en-US");
-  }
-  
+  function formatDate(date, time) {
+    const utcSeconds = jsDate(date, time).getTime() / 1000;
+    const d = new Date(0);
+    d.setUTCSeconds(utcSeconds);
+    return d.toLocaleDateString("en-US");
+  }
+
+  function formatTime(date, time) {
+    const utcSeconds = jsDate(date, time).getTime() / 1000;
+    const d = new Date(0);
+    d.setUTCSeconds(utcSeconds);
+    return d.toLocaleTimeString("en-US");
+  }  
+
   return (
     <div className="container pt-5">
       <form onSubmit={onSubmit}>
